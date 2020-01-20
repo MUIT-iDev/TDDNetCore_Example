88cb0e618727f57e6e16e601a4e2da1d00063d33
kittisak.tos <ruzerie@gmail.com>
2020-01-20 19:56:37 +0700

    add inline data

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,6 +5,7 @@ namespace TDDNetCore_Example
 {
     public class UnitTest1
     {
+        /*
         [Fact]
         public void ValidateCalculatroMethodAdd()
         {
@@ -27,6 +28,17 @@ namespace TDDNetCore_Example
             var result = Add(500, 691);
 
             Assert.Equal(1191, result);
+        }
+        */
+
+        [Theory]
+        [InlineData(5, 6, 11)]
+        [InlineData(50, 69, 119)]
+        [InlineData(500, 691, 1191)]
+        public void ValidateCalculatroMethodAdd(int frist, int second, int expected) {
+            var result = Add(frist, second);
+
+            Assert.Equal(expected, result);
         }
 
         public int Add(int a, int b)
