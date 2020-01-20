5183afee1e885c5dbc8ece03faf62b550adc44c4
kittisak.tos <ruzerie@gmail.com>
2020-01-20 19:19:30 +0700

    create test

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -6,9 +6,32 @@ namespace TDDNetCore_Example
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void ValidateCalculatroMethodAdd()
         {
+            var result = Add(5, 6);
 
+            Assert.Equal(11, result);
+        }
+
+        [Fact]
+        public void ValidateCalculatroMethodAdd2Digit()
+        {
+            var result = Add(50, 69);
+
+            Assert.Equal(119, result);
+        }
+
+        [Fact]
+        public void ValidateCalculatroMethodAdd3Digit()
+        {
+            var result = Add(500, 691);
+
+            Assert.Equal(1191, result);
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
